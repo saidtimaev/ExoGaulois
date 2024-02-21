eb726c46c6e32020d67f7f36a8fc780df0075087
saidtimaev <said.timaev@stagiaire-elan.fr>
2024-02-21 11:50:07 +0100

    ajouter fichier php qui permet d'afficher la liste des villages avec leur nombre d'habitants par ordre décroissant

---
+++ listVillages.php
@@ -0,0 +1,50 @@
+<?php
+
+include "config/mysql.php";
+include "databaseconnect.php";
+
+
+// On récupère tout le contenu de la table personnage
+$sqlQuery = '
+            SELECT nom_lieu, COUNT(nom_personnage) AS NombrePersonnages
+            FROM personnage
+            INNER JOIN lieu ON personnage.id_lieu = lieu.id_lieu
+            GROUP BY personnage.id_lieu
+            ORDER BY NombrePersonnages DESC
+            ';
+
+$villagesStatement = $mysqlClient->prepare($sqlQuery);
+$villagesStatement->execute();
+$villages = $villagesStatement->fetchAll();
+
+                
+                    // Début table
+                    echo "<table>",
+                        "<thead>",
+                            "<tr>",
+                                "<th >Nom</th>",
+                                "<th >Nombre d'habitants</th>",
+                            "</tr>",
+                        "</thead>",
+                        "<tbody>";
+                    
+                    
+
+                    // Boucle qui permet d'afficher les Gaulois
+                    foreach($villages as $village){
+                        echo "<tr>",
+                                "<td>".$village["nom_lieu"]."</td>",
+                                "<td>".$village["NombrePersonnages"]."</td>",
+                            "</tr>";
+                        
+                       
+                        
+
+                    }
+                    echo    "</tbody>",
+                            "</table";
+                            // Fin tableau
+
+                            
+                
+            ?>
